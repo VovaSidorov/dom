e3060d365f1f98423e333134ed33b920f3628a92
VovaSidorov <VovaCivic@gmail.com>
2019-11-25 13:02:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// let message = prompt("Как вы относитесь к технике apple?");
+// let messageBlock = document.querySelector('#prompt');
+// messageBlock.innerHTML = message;
+
+let reklama = document.querySelector('.adv');
+reklama.remove();
+
+let zagolovokText = 'Мы продаем только подлинную технику Apple';
+let zagolovok = document.querySelector('.title');
+zagolovok.innerHTML = zagolovokText;
+
+var menu = document.querySelector('.menu');
+var items = menu.children;
+menu.innerHTML = 'dsfsfsdfsd';
